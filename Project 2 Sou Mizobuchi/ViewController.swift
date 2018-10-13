76b7a1d8fe002df3380f5f468122a5688183a68e
user144546 <user144546@74-80-245-30.ic2net.net>
2018-10-12 20:32:08 -0700

    Initial Commit

---
+++ Project 2 Sou Mizobuchi/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Project 2 Sou Mizobuchi
+//
+//  Created by user144546 on 10/12/18.
+//  Copyright © 2018 IS543. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
