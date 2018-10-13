14c26618b305fca69a058d22c97ab46f379c44a1
user144546 <user144546@74-80-245-30.ic2net.net>
2018-10-12 20:49:49 -0700

    collection and table view

--- Project 2 Sou Mizobuchi/Controller/TempleListViewController.swift
+++ Project 2 Sou Mizobuchi/Controller/TempleListViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  TempleListViewController.swift
 //  Project 2 Sou Mizobuchi
 //
 //  Created by user144546 on 10/12/18.
@@ -8,13 +8,12 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class TempleListViewController: UIViewController {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-
+    
+    
 }
-
